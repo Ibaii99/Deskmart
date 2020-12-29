12a9084ce91315fe6074c20416ad961b18481d3d
Ibaii99 <ibai.guillen@opendeusto.es>
2020-12-29 12:00:04 +0100

    path resolving

--- Deskmart_Backend_Logic/rp.py
+++ Deskmart_Backend_Logic/rp.py
@@ -1,8 +1,8 @@
-from grove_lib.grove_flame_sensor import Grove_Flame_Sensor
-from grove_lib.grove_infrarred_sensor import Grove_Infrarred_Sensor
-from grove_lib.grove_rgb_lcd import Grove_Rgb_Lcd
-from grove_lib.grove_temphum_sensor import Grove_TempHum_sensor
-from grove_lib.grove_touch_sensor import Grove_Touch_Sensor
+from Deskmart_Backend_Logic.grove_lib.grove_flame_sensor import Grove_Flame_Sensor
+from Deskmart_Backend_Logic.grove_lib.grove_infrarred_sensor import Grove_Infrarred_Sensor
+from Deskmart_Backend_Logic.grove_lib.grove_rgb_lcd import Grove_Rgb_Lcd
+from Deskmart_Backend_Logic.grove_lib.grove_temphum_sensor import Grove_TempHum_sensor
+from Deskmart_Backend_Logic.grove_lib.grove_touch_sensor import Grove_Touch_Sensor
 
 import config
 
