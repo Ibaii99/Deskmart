0c61585d7c2291c90e6d0a1fac6cbd7be57f6bb2
Ibaii99 <ibai.guillen@opendeusto.es>
2021-01-04 15:38:45 +0100

    All sensors working

--- Deskmart_Backend_Logic/grove_lib/grove_flame_sensor.py
+++ Deskmart_Backend_Logic/grove_lib/grove_flame_sensor.py
@@ -1,9 +1,4 @@
-import grovepi
-
-
-
 import RPi.GPIO as GPIO
-
 
 class Grove_Flame_Sensor:
 
@@ -12,13 +7,12 @@ class Grove_Flame_Sensor:
         # self.flame_sensor = pin
         # grovepi.pinMode(self.flame_sensor,"INPUT")
         GPIO.setmode(GPIO.BCM)
-        self.INPUT_PIN = 22
+        self.INPUT_PIN = pin
         GPIO.setup(self.INPUT_PIN, GPIO.IN)
         input = GPIO.input(self.INPUT_PIN)
     
     def read(self):
         if (GPIO.input(self.INPUT_PIN) == True): # Physically read the pin now
-            return 1
-        else:
             return 0
-        #return grovepi.digitalRead(self.flame_sensor)
+        else:
+            return 1
