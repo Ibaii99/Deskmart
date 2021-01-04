48d58d1e2b407127024fdf74c7cda1723f6d77b5
Ibaii99 <ibai.guillen@opendeusto.es>
2021-01-04 15:34:03 +0100

    pins switch

--- Deskmart_Backend_Logic/grove_lib/grove_flame_sensor.py
+++ Deskmart_Backend_Logic/grove_lib/grove_flame_sensor.py
@@ -14,7 +14,7 @@ class Grove_Flame_Sensor:
         GPIO.setmode(GPIO.BCM)
         self.INPUT_PIN = 22
         GPIO.setup(self.INPUT_PIN, GPIO.IN)
-        input = GPIO.input(0)
+        input = GPIO.input(self.INPUT_PIN)
     
     def read(self):
         if (GPIO.input(self.INPUT_PIN) == True): # Physically read the pin now
