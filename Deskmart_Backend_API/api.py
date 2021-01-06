a12d5d8f0fbbb389c89c65d6c8263b0910fb1a3f
Ibaii99 <ibai.guillen@opendeusto.es>
2021-01-06 22:06:55 +0100

    influx module

--- Deskmart_Backend_API/api.py
+++ Deskmart_Backend_API/api.py
@@ -4,14 +4,14 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from datetime import datetime
 import json
-from routing import api_users
+from routing import api_users, api_sensors
 import config
 import logging
 from logic.authorization import Authorization
 
 
 app = Flask(__name__)
-#app.register_blueprint(api_markets.markets_blueprint, url_prefix=config.API_URL_PREFIX+"/market")
+app.register_blueprint(api_sensors.sensor_blueprint, url_prefix=config.API_URL_PREFIX+"/sensor")
 app.register_blueprint(api_users.users_blueprint, url_prefix=config.API_URL_PREFIX + "/user")
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -23,69 +23,69 @@ app.config['CORS_HEADERS'] = ['Content-Type', 'Authorization']
 def hello():
     return "Hello World!"
 
-@app.before_request
-def check_api_key():
-    if request.method == 'OPTIONS':
-        resp = app.make_default_options_response()
+# @app.before_request
+# def check_api_key():
+#     if request.method == 'OPTIONS':
+#         resp = app.make_default_options_response()
 
-        headers = None
-        if 'ACCESS_CONTROL_REQUEST_HEADERS' in request.headers:
-            headers = request.headers['ACCESS_CONTROL_REQUEST_HEADERS']
+#         headers = None
+#         if 'ACCESS_CONTROL_REQUEST_HEADERS' in request.headers:
+#             headers = request.headers['ACCESS_CONTROL_REQUEST_HEADERS']
 
-        h = resp.headers
+#         h = resp.headers
 
-        # Allow the origin which made the XHR
-        h['Access-Control-Allow-Origin'] = request.headers['Origin']
-        # Allow the actual method
-        h['Access-Control-Allow-Methods'] = request.headers['Access-Control-Request-Method']
-        # Allow for 10 seconds
-        h['Access-Control-Max-Age'] = "10"
+#         # Allow the origin which made the XHR
+#         h['Access-Control-Allow-Origin'] = request.headers['Origin']
+#         # Allow the actual method
+#         h['Access-Control-Allow-Methods'] = request.headers['Access-Control-Request-Method']
+#         # Allow for 10 seconds
+#         h['Access-Control-Max-Age'] = "10"
 
-        # We also keep current headers
-        if headers is not None:
-            h['Access-Control-Allow-Headers'] = headers
+#         # We also keep current headers
+#         if headers is not None:
+#             h['Access-Control-Allow-Headers'] = headers
         
-        return resp
-    else:
-        logging.warning("BEFORE")
-        # logging.warning(request.headers)
-        api_key = request.headers.get("X-Api-Key")
-        try:
-            auth = request.headers.get("Authorization")
-        except:
-            auth = None
-        if (api_key is not None and auth is not None):
-            if not Authorization.authorize_user_token(request) or not Authorization.authorize_front(api_key):  
-                abort(401)
-            else:
-                pass
-        elif (api_key is not None and auth is None):
-            if not Authorization.authorize_front(api_key) and is_get_token_route(request.url_rule):
-                abort(401)
-            else: 
-                pass
-        else:
-            abort(403)
+#         return resp
+#     else:
+#         logging.warning("BEFORE")
+#         # logging.warning(request.headers)
+#         api_key = request.headers.get("X-Api-Key")
+#         try:
+#             auth = request.headers.get("Authorization")
+#         except:
+#             auth = None
+#         if (api_key is not None and auth is not None):
+#             if not Authorization.authorize_user_token(request) or not Authorization.authorize_front(api_key):  
+#                 abort(401)
+#             else:
+#                 pass
+#         elif (api_key is not None and auth is None):
+#             if not Authorization.authorize_front(api_key) and is_get_token_route(request.url_rule):
+#                 abort(401)
+#             else: 
+#                 pass
+#         else:
+#             abort(403)
 
-@app.after_request
-def save_history(response):
-    logging.warning("After")
+# @app.after_request
+# def save_history(response):
+#     logging.warning("After")
 
-    # logging.warning(response)
-    # logging.warning(request)
-    # logging.warning(request.headers)
-    # logging.warning(request.cookies)
-    # logging.warning(request.args)
-    # logging.warning(request.url_rule)   
-    return response
+#     # logging.warning(response)
+#     # logging.warning(request)
+#     # logging.warning(request.headers)
+#     # logging.warning(request.cookies)
+#     # logging.warning(request.args)
+#     # logging.warning(request.url_rule)   
+#     return response
 
-def is_get_token_route(route):
-    login_route= "/api/v1/user/login"
-    register_route= "/api/v1/user/register"
-    if route == login_route or route == register_route:
-        return True
-    else:
-        return False
+# def is_get_token_route(route):
+#     login_route= "/api/v1/user/login"
+#     register_route= "/api/v1/user/register"
+#     if route == login_route or route == register_route:
+#         return True
+#     else:
+#         return False
 
 
 @app.errorhandler(403)
--- Deskmart_Backend_API/routing/api_sensors.py
+++ Deskmart_Backend_API/routing/api_sensors.py
@@ -1,32 +1,18 @@
-from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
-import config
+from flask import Blueprint, Response, request, abort, jsonify
+from flask_cors import cross_origin
+
+from controllers.influxdb_controller import InfluxController
+
+sensor_blueprint = Blueprint("sensor", __name__)
 
 
-class InfluxController:
-    def __init__(self):
-        self.org = "jorge.elbusto@opendeusto.es"
-        self.bucket = "DeskMart"
-        self.client = InfluxDBClient(url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=config.INFLUXDB_TOKEN)
-        
-    
-    def get_influx_data(self):
-        query = 'from(bucket: "DeskMart") |> range(start: -12h, stop: now()) |> filter(fn: (r) => r["_measurement"] == "Capacitors" or r["_measurement"] == "humTemp" or r["_measurement"] == "llama")' #FALLA LA QUERY
-        result = self.client.query_api().query(query, org=self.org)
-        results = []
-        for table in result:
-            for record in table.records:
-                results.append((record.get_time().strftime("%m/%d/%Y, %H:%M:%S"), record.get_field(), record.get_value()))
+@sensor_blueprint.route("/", methods=["GET"])
+def echo():
+    return ("Sensor module")
 
-        results_ordenados = sorted(results, key=lambda tup: tup[0])
+@sensor_blueprint.route("/all", methods=["GET"])
+@cross_origin()
+def get_all_sensors():
+    db = InfluxController()
+    return jsonify(db.get_influx_data()), 200
 
-        for x in results_ordenados:
-            print(x)
-
-        return results_ordenados
-
-
-    def get_last_timestamp(self):
-        results = getInfluxData()
-        lastSensors = results.slice(Math.max(arr.length - 7, 1))
-        return lastSensors
