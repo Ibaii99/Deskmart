0e8909780086c8a02a5d59667a0da53c3bfb48a3
Ibaii99 <ibai.guillen@opendeusto.es>
2021-01-07 16:44:32 +0100

    name binding

--- Deskmart_Backend_API/routing/api_sensors.py
+++ Deskmart_Backend_API/routing/api_sensors.py
@@ -14,35 +14,35 @@ def echo():
 @sensor_blueprint.route("/all", methods=["GET"])
 @cross_origin()
 def get_all_sensors():
-    return jsonify(db.get_influx_data("username")), 200
+    return jsonify(db.get_influx_data("rp-jorge-ibai")), 200
 
 @sensor_blueprint.route("/last", methods=["GET"])
 @cross_origin()
 def get_last_upload():
-    return jsonify(db.get_last_timestamp("username")), 200
+    return jsonify(db.get_last_timestamp("rp-jorge-ibai")), 200
 
 @sensor_blueprint.route("/last/flame", methods=["GET"])
 @cross_origin()
 def get_last_flame():
-    return jsonify(db.get_last_flame("username")), 200
+    return jsonify(db.get_last_flame("rp-jorge-ibai")), 200
 
 @sensor_blueprint.route("/last/humidity", methods=["GET"])
 @cross_origin()
 def get_last_humidity():
-    return jsonify(db.get_last_hum("username")), 200
+    return jsonify(db.get_last_hum("rp-jorge-ibai")), 200
 
 @sensor_blueprint.route("/last/temperature", methods=["GET"])
 @cross_origin()
 def get_last_temperature():
-    return jsonify(db.get_last_temp("username")), 200
+    return jsonify(db.get_last_temp("rp-jorge-ibai")), 200
 
 @sensor_blueprint.route("/heatmap/value", methods=["GET"])
 @cross_origin()
 def get_heatmap_value():
-    return jsonify(db.get_capacitors_heatmap("username")), 200
+    return jsonify(db.get_capacitors_heatmap("rp-jorge-ibai")), 200
 
 @sensor_blueprint.route("/heatmap/color", methods=["GET"])
 @cross_origin()
 def get_heatmap_color():
-    return jsonify(db.get_heatmap_colors("username")), 200
+    return jsonify(db.get_heatmap_colors("rp-jorge-ibai")), 200
 
